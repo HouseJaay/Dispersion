c21004f86a399c80aba9101ddd6e414fffd45664
HouseJaay <shijiehao95@126.com>
2023-04-14 18:10:59 +0800

    serial

--- ftan.py
+++ ftan.py
@@ -1,13 +1,12 @@
 import numpy as np
 import sys, os
 import importlib
-from scipy.signal import hilbert
+from scipy.signal import hilbert, kaiserord, firwin, filtfilt
 from scipy.interpolate import interp1d
 from scipy import fftpack
 from obspy.geodetics.base import gps2dist_azimuth
 from glob import glob
-from os.path import join
-import matplotlib.pyplot as plt
+from os.path import join, basename
 
 try:
     proj_path = sys.argv[1]
@@ -43,7 +42,7 @@ def read_data(fpath):
 def cos_window(npts, n1, n2):
     window = np.zeros(npts)
     window[n1:n2] = 1
-    taper_n = round(n2-n1)*0.05
+    taper_n = round((n2-n1)*0.05)
     window[n1:(n1+taper_n)] = np.sin(0.5*np.pi*np.arange(taper_n)/taper_n)
     window[(n2-taper_n):n2] = np.sin(0.5*np.pi*np.arange(taper_n,0,-1)/taper_n)
     return window
@@ -55,20 +54,104 @@ falpha = interp1d([0, 100, 250, 500, 1000, 2000, 4000, 20000],
 def gauss_window(alpha, freqs, cf):
     return np.exp(-alpha * ((freqs-cf)/cf)**2)
 
-def envelope_image(signal, samplef, periods, dist):
+def envelope_image(par, time, signal, delta, dist):
+    periods = par.periods
     alpha = falpha(dist)
-    image = np.zeros(len(periods), len(signal))
+    image = np.zeros([len(periods), len(signal)])
+    P, T = np.meshgrid(periods, time, indexing='ij')
+    VG = dist / T
+    npts = len(signal)
     nfft = fftpack.next_fast_len(len(signal))
     fft = fftpack.rfft(signal, nfft)
+    freqs = fftpack.rfftfreq(nfft, delta)
     for ip in range(len(periods)):
         cf = 1.0 / periods[ip]
+        gs = gauss_window(alpha, freqs, cf)
+        signal_f = fftpack.irfft(fft * gs, nfft)
+        env_f = np.abs(hilbert(signal_f))
+        env_f /= np.max(env_f)
+        image[ip, :] = env_f[:npts]
+    return P, VG, image
+
+def phase_image(par, time, signal, samplef, dist):
+    periods = par.periods
+    numtaps, beta = kaiserord(par.ripple, par.width/(0.5*samplef))
+    image = np.zeros([len(periods), len(signal)])
+    P, T = np.meshgrid(periods, time, indexing='ij')
+    VP = dist / (T - P/8)
+    for ip in range(len(periods)):
+        cf = 1.0 / periods[ip]
+        w = par.band_pass_width / 2.0
+        lc = 1.0 / (periods[ip]+w)
+        hc = 1.0 / (periods[ip]-w)
+        taps = firwin(numtaps, [lc, hc], window=('kaiser', beta), fs=samplef, pass_zero='bandpass')
+        print(len(taps), len(signal))
+        signal_filtered = filtfilt(taps, 1, signal, padtype=None)
+        signal_filtered /= np.max(signal_filtered)
+        image[ip, :] = signal_filtered
+    return P, VP, image
+
+def nearest_max(data, ini):
+    """
+    Find index of nearest max
+    data: input array
+    ini: initial index
+    Return
+    isgood: boolean
+    index_max: int, result
+    """
+    if data[ini+1] == data[ini]:
+        return (False, 0)
+    flag = int((data[ini+1] - data[ini]) / abs(data[ini+1] - data[ini]))
+    iprev = ini
+    inext = ini + flag
+    while data[inext] > data[iprev]:
+        iprev += flag
+        inext += flag
+        if inext > len(data)-1 or iprev < 0:
+            return (False, 0)
+    return (True, iprev)
+
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+
+def search_image(par, Img, Vels):
+    periods = par.periods
+    maxarr = np.zeros(len(periods))
+    init_idx1 = find_nearest(periods, par.init_per)
+    init_idx2 = find_nearest(Vels[init_idx1], par.init_phav)
+    prev_idx2 = init_idx2
+    for ip in np.arange(init_idx1, len(periods)):
+        isgood, idx = nearest_max(Img[ip], prev_idx2)
+        if isgood:
+            maxarr[ip] = Vels[ip, idx]
+            prev_idx2 = idx
+    prev_idx2 = init_idx2
+    for ip in np.arange(init_idx1-1, -1, -1):
+        isgood, idx = nearest_max(Img[ip], prev_idx2)
+        if isgood:
+            maxarr[ip] = Vels[ip, idx]
+            prev_idx2 = idx
+    return maxarr
+
+def plot_phase_image(P, V, Img, out):
+    import matplotlib as mpl
+    mpl.use('Agg')
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    ax.contourf(P, V, Img, levels=50, cmap='viridis')
+    plt.savefig(out + '.png')
+    plt.close()
 
 
-all_inputs = glob(join(par.input_path, "*"))
+all_inputs = glob(join(par.input_path, "*.dat"))
 os.makedirs(par.output_path, exist_ok=True)
 if par.is_save_fig:
     os.makedirs(par.fig_path, exist_ok=True)
-
+periods = par.periods
 
 for fpath in all_inputs:
     data, dist = read_data(fpath)
@@ -83,5 +166,14 @@ for fpath in all_inputs:
     stack_egf = data[:,1] + data[:,2]
     noise = stack_egf[n2:]
     stack_egf *= window
-    envelope_image(stack_egf, samplef, par.periods, dist)
+    P, VP, PhaImg = phase_image(par, time[n1:n2], stack_egf[n1:n2], samplef, dist)
+    P2, VG, GrpImg = envelope_image(par, time[n1:n2], stack_egf[n1:n2], delta, dist)
+    pha_vels = search_image(par, PhaImg, VP)
+    op = join(par.output_path, basename(fpath)+'.disp')
+    oip = join(par.fig_path, basename(fpath))
+    np.savetxt(op, np.c_[periods, pha_vels])
+    if par.is_save_fig:
+        plot_phase_image(P, VP, PhaImg, oip)
+        plot_phase_image(P2, VG, GrpImg, oip+'.gv')
+
     
--- ftan_parameters.py
+++ ftan_parameters.py
@@ -2,12 +2,18 @@ import numpy as np
 
 # Directory of CCFs/EGFs file
 input_path = "/home/shijie/data/hawaii/DISP4/CCFs/"
-output_path = "/home/shijie/data/hawaii/DISP4/CCFs/disp_pycode/"
+output_path = "/home/shijie/data/hawaii/DISP4/disp_pycode/"
 is_save_fig = True
-fig_path = "/home/shijie/data/hawaii/DISP4/CCFs/disp_pycode/fig/"
+fig_path = "/home/shijie/data/hawaii/DISP4/disp_pycode/fig/"
 # CF for Cross-correlation function, EGF for Empirical Green's Function
 input_type = "CF" 
 # Define time window by min and max surface wave velocity (km/s)
 minv, maxv = 1.2, 4.2
 # Period band
 periods = np.arange(2.5, 10, 0.5)
+# width of band pass filter (second)
+band_pass_width = 0.5
+# minimum ratio between interstation distance and surface wave wavelength
+min_lambda_ratio = 2.0
+# Initial point of image search
+init_per, init_phav = 8, 3.0
