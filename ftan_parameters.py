65db41d3e40891a682a946881b5be5942c80396e
HouseJaay <shijiehao95@126.com>
2023-06-07 10:25:43 +0800

    mpi

--- ftan_parameters.py
+++ ftan_parameters.py
@@ -1,19 +1,21 @@
 import numpy as np
 
 # Directory of CCFs/EGFs file
-input_path = "/home/shijie/data/hawaii/DISP4/CCFs/"
-output_path = "/home/shijie/data/hawaii/DISP4/disp_pycode/"
+input_path = "/home/shijie/data/huidong/DISP_2_1/CCFs/"
+output_path = "/home/shijie/data/huidong/DISP_2_1/disp/"
 is_save_fig = True
-fig_path = "/home/shijie/data/hawaii/DISP4/disp_pycode/fig/"
+fig_path = "/home/shijie/data/huidong/DISP_2_1/disp/fig/"
 # CF for Cross-correlation function, EGF for Empirical Green's Function
 input_type = "CF" 
 # Define time window by min and max surface wave velocity (km/s)
-minv, maxv = 1.2, 4.2
+minv, maxv = 1.6, 5.0
 # Period band
-periods = np.arange(2.5, 10, 0.5)
+periods = np.arange(0.5, 3.0, 0.25)
 # width of band pass filter (second)
-band_pass_width = 0.5
+band_pass_width = 0.1
 # minimum ratio between interstation distance and surface wave wavelength
 min_lambda_ratio = 2.0
 # Initial point of image search
-init_per, init_phav = 8, 3.0
+init_per, init_phav = 2, 4.0
+# Parameters for filter design
+ripple, width = 63, 0.05
